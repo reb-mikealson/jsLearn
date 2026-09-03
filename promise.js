0d345caebe4ee34c5970bb5b78cd7039fc3bc129
reb-mikealson <pratharajgupta@gmail.com>
2026-09-03 14:22:57 +0530

    promises done

--- promise.js
+++ promise.js
@@ -2,6 +2,7 @@ function f1(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
         console.log("hiiiiiii");
+        resolve();
     },4000);
     })
 }
@@ -10,6 +11,12 @@ function f2(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
         console.log("ABES COLLEGE");
+        resolve();
     },2000);
     })
 }
+
+f1().then(f2)
+    .catch((err) => {
+        console.log(err);
+    })
